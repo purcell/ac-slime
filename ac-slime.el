42845f99c7ed4706f193ab6a960c8a18a90a2053
Steve Purcell <steve@sanityinc.com>
2011-06-24 19:40:02 +0100

    Improve sample initialization code

--- ac-slime.el
+++ ac-slime.el
@@ -7,6 +7,8 @@
 ;;   (require 'ac-slime)
 ;;   (add-hook 'slime-mode-hook 'set-up-slime-ac)
 ;;   (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+;;   (eval-after-load "auto-complete"
+;;     '(add-to-list 'ac-modes 'slime-repl-mode))
 ;;----------------------------------------------------------------------------
 
 (eval-when-compile (require 'cl))
