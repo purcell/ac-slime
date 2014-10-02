0778c261c73d8f12f235f4e42b11eb1086e656e3
Steve Purcell <steve@sanityinc.com>
2014-10-02 12:41:50 +0100

    Add copyright and licence info

--- ac-slime.el
+++ ac-slime.el
@@ -1,10 +1,27 @@
 ;;; ac-slime.el --- An auto-complete source using slime completions
-;;
+
+;; Copyright (C) 2010-2014  Steve Purcell
+
 ;; Author: Steve Purcell <steve@sanityinc.com>
 ;; URL: https://github.com/purcell/ac-slime
 ;; Version: DEV
 ;; Package-Requires: ((auto-complete "1.4") (slime "2.9"))
-;;
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; Usage:
